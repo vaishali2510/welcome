7642a82853e882e47f304d58458e6110bbc58c14
Metin Senturk <metinsenturk@me.com>
2020-11-02 21:42:43 -0500

    more attributes for person

--- greeter/people.py
+++ greeter/people.py
@@ -2,6 +2,8 @@
 A list of students partipating to the class.
 """
 from dataclasses import dataclass
+from datetime import date
+from typing import Dict, List
 
 
 @dataclass
@@ -11,6 +13,9 @@ class Person:
     """
     name: str
     lastname: str
+    date_of_birth: date
+    educational_history: List[str]
+    skills: Dict[str, str]
 
     def __repr__(self) -> str:
         return '{}, {}'.format(self.lastname.capitalize(), self.name.capitalize())
@@ -20,4 +25,10 @@ def get_metin_senturk() -> Person:
     """
     Information on instructor. 
     """
-    return Person(name='metin', lastname='senturk')
+    return Person(
+        name='metin',
+        lastname='senturk',
+        date_of_birth=date(1989, 9, 6),
+        educational_history=['Bilkent University', "Saint Peter's University"],
+        skills=dict(python='*****', sql='**')
+    )
