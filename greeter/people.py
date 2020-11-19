b734b58d5640b1373b5d70c18bb45a93742dd68b
Metin Senturk <metinsenturk@me.com>
2020-11-18 22:48:27 -0500

    add person to peoples list

--- greeter/people.py
+++ greeter/people.py
@@ -32,3 +32,15 @@ def get_metin_senturk() -> Person:
         educational_history=['Bilkent University', "Saint Peter's University"],
         skills=dict(python='*****', sql='**')
     )
+
+def get_person1_edited_this_code() -> Person:
+    """
+    Information on instructor. 
+    """
+    return Person(
+        name='murat',
+        lastname='senturk',
+        date_of_birth=date(1989, 9, 6),
+        educational_history="Saint Peter's University",
+        skills=dict(python='*****', csharp='****')
+    )
